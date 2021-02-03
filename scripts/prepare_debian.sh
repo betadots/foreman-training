fb0fd333eac699611ab3da59b7382efa36c83b63
Martin Alfke <ma@example42.com>
2021-02-03 11:27:07 +0100

    script fuer workstation vorberteitung (vgarant, git, plugins, box)

---
+++ scripts/prepare_debian.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo cat <<EOF > /etc/apt/sources.list
+deb http://ftp.de.debian.org/debian buster main contrib non-free
+#deb-src http://ftp.de.debian.org/debian buster main contrib non-free
+
+deb http://ftp.de.debian.org/debian-security buster/updates main contrib non-free
+#deb-src http://deb.debian.org/debian buster/updates main contrib non-free
+
+deb http://ftp.de.debian.org/debian buster-updates main contrib non-free
+#deb-src http://debian/debian buster-updates main contrib non-free
+EOF
+
+sudo apt-get update
+sudo apt-get install -y git
+
+wget https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
+sudo dpkg -i vagrant_2.2.14_x86_64.deb
+
+vagrant plugin install vagrant-hostmanager
+vagrant plugin install vagrant-vbguest
+
+echo "Wenn nach dem Provider grfragt wird, bitte VirtualBox auswaehlen"
+vagrant box add centos/7
+
